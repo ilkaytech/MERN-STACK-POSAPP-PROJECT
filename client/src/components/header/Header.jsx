f9f7b8fd3bc9301951b0c3c60a43c4ac11a49eac
ilkay <06ilkayozturk@gmail.com>
2023-11-29 02:05:52 +0300

    feat:header is created

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,16 +1,58 @@
-import React from "react";
+import {
+  SearchOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  CopyOutlined,
+  UserOutlined,
+  BarChartOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+
+import { Input } from "antd";
 
 const Header = () => {
   return (
-    <div>
-      <header>
+    <div className="border-b mb-6">
+      <header className="py-4 px-6 flex justify-between items-center gap-10">
         <div className="logo">
           <a href="/">
             <h2 className="text-2xl font-bold md:text-4xl ">LOGO</h2>
           </a>
         </div>
-        <div className="header-search"></div>
-        <div className="menu-links"></div>
+        <div className="header-search flex-1">
+          <Input
+            size="large"
+            placeholder="Search..."
+            prefix={<SearchOutlined />}
+            className="rounded-full max-w-[800px]"
+          />
+        </div>
+        <div className="menu-links flex justify-between items-center gap-8 ">
+          <a href={"/"} className="menu-link flex flex-col">
+            <HomeOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px] ">Ana Sayfa</span>
+          </a>
+          <a href={"/"} className="menu-link flex flex-col">
+            <ShoppingCartOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px] ">Sepet</span>
+          </a>
+          <a href={"/"} className="menu-link flex flex-col">
+            <CopyOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px] ">Faturalar</span>
+          </a>
+          <a href={"/"} className="menu-link flex flex-col">
+            <UserOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[30px]">Müşteriler</span>
+          </a>
+          <a href={"/"} className="menu-link flex flex-col">
+            <BarChartOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px] ">İstatistikler</span>
+          </a>
+          <a href={"/"} className="menu-link flex flex-col">
+            <LogoutOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px] ">Çıkış</span>
+          </a>
+        </div>
       </header>
     </div>
   );
