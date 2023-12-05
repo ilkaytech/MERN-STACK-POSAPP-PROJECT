5d10e19a3f12ecf3a30b051041c2429da3cbea68
ilkay <06ilkayozturk@gmail.com>
2023-12-05 12:20:58 +0300

    fix:categories is edit

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,11 +1,53 @@
 const Categories = () => {
   return (
-    <ul>
+    <ul className="flex gap-4 flex-col text-lg">
       <li
         className="bg-primary px-6 py-10 text-white cursor-pointer 
-      hover:bg-secondary transition-all "
+      hover:bg-secondary transition-all text-center"
       >
         <span>Tümü</span>
+      </li>
+      <li
+        className="bg-secondary px-6 py-10 text-white cursor-pointer 
+      hover:bg-primary transition-all text-center"
+      >
+        <span>Yiyecek</span>
+      </li>
+      <li
+        className="bg-secondary px-6 py-10 text-white cursor-pointer 
+      hover:bg-primary transition-all text-center"
+      >
+        <span>İçecek</span>
+      </li>
+      <li
+        className="bg-secondary px-6 py-10 text-white cursor-pointer 
+      hover:bg-primary transition-all text-center"
+      >
+        <span>Meyve</span>
+      </li>
+      <li
+        className="bg-secondary px-6 py-10 text-white cursor-pointer 
+      hover:bg-primary transition-all text-center"
+      >
+        <span>Sebze</span>
+      </li>
+      <li
+        className="bg-secondary px-6 py-10 text-white cursor-pointer 
+      hover:bg-primary transition-all text-center"
+      >
+        <span>Kıyafet</span>
+      </li>
+      <li
+        className="bg-secondary px-6 py-10 text-white cursor-pointer 
+      hover:bg-primary transition-all text-center"
+      >
+        <span>+</span>
+      </li>
+      <li
+        className="bg-secondary px-6 py-10 text-white cursor-pointer 
+      hover:bg-primary transition-all text-center"
+      >
+        <span>Düzenle</span>
       </li>
     </ul>
   );
