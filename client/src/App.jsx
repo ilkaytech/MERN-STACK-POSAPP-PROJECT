96fe9ffd3c99c9d956d52bf1245aac6f1f3a646d
ilkay <06ilkayozturk@gmail.com>
2023-12-05 12:18:14 +0300

    feat:products categories is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
+import Products from "./components/products/Products";
 
 function App() {
   return (
     <>
       <Header />
-      <div className="home px-6 flex justify-between ">
-        <div className="categories">
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-107px)] pb-64">
           <Categories />
         </div>
-        <div className="products">
-          <div className="">products</div>
+        <div className="products flex-[8] ">
+          <Products />
         </div>
         <div className="">
           <div>cart totals</div>
---
+++ client/src/components/products/Products.jsx
@@ -0,0 +1,125 @@
+const Products = () => {
+  return (
+    <div className="products-wrapper grid grid-cols-card gap-4">
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none rounded-b-md ">
+        <div className="product-img">
+          <img
+            src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg"
+            alt=""
+            className="h-34 object-cover w-full border-b-4 border-slate-300"
+          />
+        </div>
+        <div className="product-info flex flex-col p-2">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none rounded-b-md ">
+        <div className="product-img">
+          <img
+            src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg"
+            alt=""
+            className="h-34 object-cover w-full border-b-4 border-slate-300"
+          />
+        </div>
+        <div className="product-info flex flex-col p-2">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none rounded-b-md ">
+        <div className="product-img">
+          <img
+            src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg"
+            alt=""
+            className="h-34 object-cover w-full border-b-4 border-slate-300"
+          />
+        </div>
+        <div className="product-info flex flex-col p-2">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none rounded-b-md ">
+        <div className="product-img">
+          <img
+            src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg"
+            alt=""
+            className="h-34 object-cover w-full border-b-4 border-slate-300"
+          />
+        </div>
+        <div className="product-info flex flex-col p-2">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none rounded-b-md ">
+        <div className="product-img">
+          <img
+            src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg"
+            alt=""
+            className="h-34 object-cover w-full border-b-4 border-slate-300"
+          />
+        </div>
+        <div className="product-info flex flex-col p-2">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none rounded-b-md ">
+        <div className="product-img">
+          <img
+            src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg"
+            alt=""
+            className="h-34 object-cover w-full border-b-4 border-slate-300"
+          />
+        </div>
+        <div className="product-info flex flex-col p-2">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none rounded-b-md ">
+        <div className="product-img">
+          <img
+            src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg"
+            alt=""
+            className="h-34 object-cover w-full border-b-4 border-slate-300"
+          />
+        </div>
+        <div className="product-info flex flex-col p-2">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none rounded-b-md ">
+        <div className="product-img">
+          <img
+            src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg"
+            alt=""
+            className="h-34 object-cover w-full border-b-4 border-slate-300"
+          />
+        </div>
+        <div className="product-info flex flex-col p-2">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none rounded-b-md ">
+        <div className="product-img">
+          <img
+            src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg"
+            alt=""
+            className="h-34 object-cover w-full border-b-4 border-slate-300"
+          />
+        </div>
+        <div className="product-info flex flex-col p-2">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
