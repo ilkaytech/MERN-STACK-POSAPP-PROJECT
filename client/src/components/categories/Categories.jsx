ee711999b51c278798d281aac0c63e2e4081ea9e
ilkay <06ilkayozturk@gmail.com>
2023-12-07 19:58:52 +0300

    feat:responsive

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,51 +1,51 @@
 const Categories = () => {
   return (
-    <ul className="flex gap-4 flex-col text-lg">
+    <ul className="flex gap-4 md:flex-col text-lg">
       <li
         className="bg-primary px-6 py-10 text-white cursor-pointer 
-      hover:bg-secondary transition-all text-center"
+      hover:bg-secondary transition-all text-center min-w-[145px]"
       >
         <span>Tümü</span>
       </li>
       <li
         className="bg-secondary px-6 py-10 text-white cursor-pointer 
-      hover:bg-primary transition-all text-center"
+      hover:bg-primary transition-all text-center min-w-[145px]"
       >
         <span>Yiyecek</span>
       </li>
       <li
         className="bg-secondary px-6 py-10 text-white cursor-pointer 
-      hover:bg-primary transition-all text-center"
+      hover:bg-primary transition-all text-center min-w-[145px]"
       >
         <span>İçecek</span>
       </li>
       <li
         className="bg-secondary px-6 py-10 text-white cursor-pointer 
-      hover:bg-primary transition-all text-center"
+      hover:bg-primary transition-all text-center min-w-[145px]"
       >
         <span>Meyve</span>
       </li>
       <li
         className="bg-secondary px-6 py-10 text-white cursor-pointer 
-      hover:bg-primary transition-all text-center"
+      hover:bg-primary transition-all text-center min-w-[145px]"
       >
         <span>Sebze</span>
       </li>
       <li
         className="bg-secondary px-6 py-10 text-white cursor-pointer 
-      hover:bg-primary transition-all text-center"
+      hover:bg-primary transition-all text-center min-w-[145px]"
       >
         <span>Kıyafet</span>
       </li>
       <li
         className="bg-secondary px-6 py-10 text-white cursor-pointer 
-      hover:bg-primary transition-all text-center"
+      hover:bg-primary transition-all text-center min-w-[145px]"
       >
         <span>+</span>
       </li>
       <li
         className="bg-secondary px-6 py-10 text-white cursor-pointer 
-      hover:bg-primary transition-all text-center"
+      hover:bg-primary transition-all text-center min-w-[145px]"
       >
         <span>Düzenle</span>
       </li>
