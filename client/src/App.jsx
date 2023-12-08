3c02aa2de70ddc0f7e52e9e8308dadd9d7f64be1
ilkay <06ilkayozturk@gmail.com>
2023-12-08 23:42:03 +0300

    feat:react root is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,15 @@
-import CartTotals from "./components/cart/CartTotals";
-import Categories from "./components/categories/Categories";
-import Header from "./components/header/Header";
-import Products from "./components/products/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePages from "./pages/HomePages";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className="home px-6 flex flex-col md:flex-row justify-between gap-10 md:pb-0 pb-24">
-        <div className="categories overflow-auto max-h-[calc(100vh_-_107px)] md:pb-10  ">
-          <Categories />
-        </div>
-        <div className="products flex-[8] max-h-[calc(100vh_-_107px)] overflow-auto ">
-          <Products />
-        </div>
-        <div className="card-wrapper min-w-[300px] md:-mr[24px] md:-mt[24px] border">
-          <CartTotals />
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePages />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/HomePages.jsx
@@ -0,0 +1,25 @@
+import CartTotals from "../components/cart/CartTotals";
+import Categories from "../components/categories/Categories";
+import Header from "../components/header/Header";
+import Products from "../components/products/Products";
+
+const HomePages = () => {
+  return (
+    <>
+      <Header />
+      <div className="home px-6 flex flex-col md:flex-row justify-between gap-10 md:pb-0 pb-24">
+        <div className="categories overflow-auto max-h-[calc(100vh_-_107px)] md:pb-10  ">
+          <Categories />
+        </div>
+        <div className="products flex-[8] max-h-[calc(100vh_-_107px)] overflow-auto ">
+          <Products />
+        </div>
+        <div className="card-wrapper min-w-[300px] md:-mr[24px] md:-mt[24px] border">
+          <CartTotals />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePages;
