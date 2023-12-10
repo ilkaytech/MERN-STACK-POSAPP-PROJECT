f7901b0b432baa8bee9c29e0aee4be1947c6758c
ilkay <06ilkayozturk@gmail.com>
2023-12-10 20:09:34 +0300

    feat:Created Bill-component-is-created

---
+++ client/src/components/cart/CreateBill.jsx
@@ -0,0 +1,75 @@
+import { Button, Card, Form, Input, Modal, Select } from "antd";
+
+const CreateBill = ({ isModalOpen, setIsModalOpen }) => {
+  const onFinish = (values) => {
+    console.log("Received values of form", values);
+  };
+
+  return (
+    <Modal
+      title="Fatura Oluştur"
+      open={isModalOpen}
+      footer={false}
+      onCancel={() => setIsModalOpen(false)}
+    >
+      <Form layout={"vertical"} onFinish={onFinish}>
+        <Form.Item
+          label="Müşteri Adı"
+          name={"curtomerName"}
+          rules={[
+            {
+              required: true,
+              message: "Username is required",
+            },
+          ]}
+        >
+          <Input placeholder="Bir Müşteri Adı Yazınız" />
+        </Form.Item>
+        <Form.Item
+          rules={[{ required: true }]}
+          name={"customerPhoneNumber"}
+          label="Tel No"
+        >
+          <Input placeholder="Bir Tel No Yazınız" maxLength={11} />
+        </Form.Item>
+        <Form.Item
+          label="Ödeme Yöntemi"
+          rules={[{ required: true }]}
+          name={"paymentMode"}
+        >
+          <Select placeholder="Ödeme Yöntemi Seçiniz">
+            <Select.Option value="Nakit">Nakit</Select.Option>
+            <Select.Option value="Kredi Kartı">Kredi Kartı</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Card className="">
+          <div className="flex justify-between">
+            <span>Ara Toplam</span>
+            <span>549.00€</span>
+          </div>
+          <div className="flex justify-between my-2">
+            <span>KDV Toplam %8</span>
+            <span className="text-red-600 ">+43.92€</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Toplam</span>
+            <span>592.92€</span>
+          </div>
+          <div className="flex justify-end">
+            <Button
+              className="mt-4 "
+              type="primary"
+              onClick={() => setIsModalOpen(true)}
+              htmlType="submit"
+            >
+              Sipariş oluştur
+            </Button>
+          </div>
+        </Card>
+      </Form>
+    </Modal>
+  );
+};
+
+export default CreateBill;
