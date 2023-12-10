68e945ca0770afe3a6a0f3987fdc6071a2f6af1d
ilkay <06ilkayozturk@gmail.com>
2023-12-10 20:10:11 +0300

    feat:cartPage-is-created

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,3 +1,5 @@
+import { Badge, Input } from "antd";
+import { Link } from "react-router-dom";
 import {
   SearchOutlined,
   HomeOutlined,
@@ -7,8 +9,6 @@ import {
   BarChartOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
-
-import { Badge, Input } from "antd";
 
 const Header = () => {
   return (
@@ -32,59 +32,59 @@ const Header = () => {
         fixed z-50 bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0
         border-t md:px-0 px-4 py-1"
         >
-          <a
-            href={"/"}
+          <Link
+            to={"/"}
             className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all"
           >
             <HomeOutlined className="md:text-2xl text-xl " />
             <span className="md:text-xs text-[10px] ">Ana Sayfa</span>
-          </a>
+          </Link>
           <Badge count={5} offset={[0, 6]} className="md:flex hidden">
-            <a
-              href={"/"}
+            <Link
+              to={"/cart"}
               className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all"
             >
               <ShoppingCartOutlined className="md:text-2xl text-xl" />
               <span className="md:text-xs text-[10px] ">Sepet</span>
-            </a>
+            </Link>
           </Badge>
-          <a
+          <Link
             href={"/"}
             className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all"
           >
             <CopyOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px] ">Faturalar</span>
-          </a>
-          <a
+          </Link>
+          <Link
             href={"/"}
             className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all"
           >
             <UserOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Müşteriler</span>
-          </a>
-          <a
+          </Link>
+          <Link
             href={"/"}
             className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all"
           >
             <BarChartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px] ">İstatistikler</span>
-          </a>
-          <a
+          </Link>
+          <Link
             href={"/"}
             className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all"
           >
             <LogoutOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px] ">Çıkış</span>
-          </a>
+          </Link>
         </div>
         <Badge count={5} offset={[0, 6]} className="md:hidden flex">
-          <a
+          <Link
             href={"/"}
             className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all"
           >
             <ShoppingCartOutlined className="text-2xl" />
             <span className="md:text-xs text-[10px] ">Sepet</span>
-          </a>
+          </Link>
         </Badge>
       </header>
     </div>
--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,5 +1,85 @@
+import { Table, Button, Card } from "antd";
+import { useState } from "react";
+import Header from "../components/header/Header";
+import CreateBill from "../components/cart/CreateBill";
+
 const CartPage = () => {
-  return <div>CartPage</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
+  return (
+    <>
+      <Header />
+      <div className="px-6">
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          bordered
+          pagination={false}
+        />
+        <div className="cart-total flex justify-end mt-4">
+          <Card className="w-72">
+            <div className="flex justify-between">
+              <span>Ara Toplam</span>
+              <span>549.00€</span>
+            </div>
+            <div className="flex justify-between my-2">
+              <span>KDV Toplam %8</span>
+              <span className="text-red-600 ">+43.92€</span>
+            </div>
+            <div className="flex justify-between">
+              <span>Toplam</span>
+              <span>592.92€</span>
+            </div>
+            <Button
+              className="mt-4 w-full"
+              type="primary"
+              size="large"
+              onClick={() => setIsModalOpen(true)}
+            >
+              Sipariş oluştur
+            </Button>
+          </Card>
+          <CreateBill
+            isModalOpen={isModalOpen}
+            setIsModalOpen={setIsModalOpen}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CartPage;
