2f14bead706ca6eddaa281ed140104f46f73d310
ilkay <06ilkayozturk@gmail.com>
2023-12-17 22:23:15 +0300

    fix:

--- api/src/configs/dbConnection.js
+++ api/src/configs/dbConnection.js
@@ -9,14 +9,8 @@ const mongoose = require("mongoose");
 const dbConnection = function () {
   // Connect:
   mongoose
-    .connect(process.env.MONGODB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("* DB Connected * ");
-      // require("../helpers/sync")(); // !!! It clear database.
-    })
+    .connect(process.env.MONGODB)
+    .then(() => console.log("* DB Connected * "))
     .catch((err) => console.log("* DB Not Connected * ", err));
 };
 
