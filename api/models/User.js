d77f99f78923ff93d7e261a555ac210224702e09
ilkay <06ilkayozturk@gmail.com>
2024-01-24 16:17:02 +0300

    user model is created

---
+++ api/models/User.js
@@ -0,0 +1,22 @@
+"use strict";
+/* --------------------------------------
+    NODEJS EXPRESS | POS API
+----------------------------------------- */
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+  {
+    username: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    password: { type: String, require: true },
+    // verify: { type: Boolean, require: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/* ------------------------------------------------------- */
+const User = mongoose.model("users", UserSchema);
+module.exports = User;
