dcf3dc7d2d844ca5b2b6d99e5eb0ade97e10bd95
ilkay <06ilkayozturk@gmail.com>
2024-01-24 12:19:18 +0300

    Category model is created

---
+++ api/models/Category.js
@@ -0,0 +1,18 @@
+"use strict";
+/* --------------------------------------
+    NODEJS EXPRESS | POS API
+----------------------------------------- */
+const mongoose = require("mongoose");
+
+const CategorySchema = mongoose.Schema(
+  {
+    title: { type: String, require: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/* ------------------------------------------------------- */
+const Category = mongoose.model("categories", CategorySchema);
+module.exports = Category;
