332f7ec6036543acfcde8b386e27db4a074cad74
ilkay <06ilkayozturk@gmail.com>
2024-01-26 13:23:10 +0300

    commit

--- client/src/components/products/edit.jsx
+++ client/src/components/products/edit.jsx
@@ -11,9 +11,7 @@ const Edit = () => {
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const res = await fetch(
-          process.env.REACT_APP_SERVER_URL + "/api/products/get-all"
-        );
+        const res = await fetch(process.env.REACT_APP_SERVER_URL + "/api/products/get-all");
         const data = await res.json();
         setProducts(data);
       } catch (error) {
@@ -27,9 +25,7 @@ const Edit = () => {
   useEffect(() => {
     const getCategories = async () => {
       try {
-        const res = await fetch(
-          process.env.REACT_APP_SERVER_URL + "/api/categories/get-all"
-        );
+        const res = await fetch(process.env.REACT_APP_SERVER_URL + "/api/categories/get-all");
         const data = await res.json();
         data &&
           setCategories(
@@ -70,14 +66,11 @@ const Edit = () => {
   const deleteCategory = (id) => {
     if (window.confirm("Emin misiniz?")) {
       try {
-        fetch(
-          process.env.REACT_APP_SERVER_URL + "/api/products/delete-product",
-          {
-            method: "DELETE",
-            body: JSON.stringify({ productId: id }),
-            headers: { "Content-type": "application/json; charset=UTF-8" },
-          }
-        );
+        fetch(process.env.REACT_APP_SERVER_URL + "/api/products/delete-product", {
+          method: "DELETE",
+          body: JSON.stringify({ productId: id }),
+          headers: { "Content-type": "application/json; charset=UTF-8" },
+        });
         message.success("Ürün başarıyla silindi.");
         setProducts(products.filter((item) => item._id !== id));
       } catch (error) {
